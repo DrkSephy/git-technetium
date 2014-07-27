18753eea0312b960409106492fcba55b9c0e3202
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-26 22:24:29 -0400

    Adding minimialistic server.js

---
+++ git-technetium/server.js
@@ -0,0 +1,22 @@
+// Require all needed modules
+var express = require('express');
+
+// Create the Express application
+var app = express();
+
+// Get an instance of the Express Router
+var router = express.Router();
+
+// Register routes of Router
+require('./routes')(router);
+
+// Prefix all routes with /api
+app.use('/api', router);
+
+// Set the location of static files
+app.use(express.static('./public'));
+
+// Start application server
+app.listen(8080, '127.0.0.1', function(){
+    console.log('Express server started on 127.0.0.1:8080');
+});
