c02f5c75e8f47f3f6a05e08902303c0004f83d54
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-27 11:39:12 -0400

    Hooked issues factory to issues controller.

--- git-technetium/public/scripts/controllers/issuesController.js
+++ git-technetium/public/scripts/controllers/issuesController.js
@@ -1,5 +1,8 @@
 'use strict';
 
-gitApp.controller('issuesController', function($scope) {
-    $scope.pageData = 'Issues';
+gitApp.controller('issuesController', function($scope, issuesFactory) {
+    $scope.pageData = [];
+    $scope.pageData = issuesFactory.get().success(function(data) {
+        $scope.pageData = data;
+    });
 });
