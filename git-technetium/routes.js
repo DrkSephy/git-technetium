b362797687cd4f812b229dbd0a083192337eb0f0
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-28 21:35:16 -0400

    Intermediate work on express route for commit totals

--- git-technetium/routes.js
+++ git-technetium/routes.js
@@ -27,4 +27,16 @@ module.exports = function(router, request) {
             }
         });
     });
+
+    router.get('/commits', function(req, res){
+        request({
+            url: 'https://api.github.com/repos/DrkSephy/WaterEmblem/stats/contributors',
+            headers: { 'user-agent': 'git-technetium' },
+            json: true
+        }, function(error, response, body){
+            if(!error && response.statusCode === 200){
+                res.send(body);
+            }
+        });
+    });
 }
