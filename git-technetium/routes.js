4cbda4b983cc76c79cec9dc682f2ead7e56e688a
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-29 18:51:48 -0400

    Added server logic for getting number of opened issues per contributor.

--- git-technetium/routes.js
+++ git-technetium/routes.js
@@ -30,12 +30,43 @@ module.exports = function(router, request) {
 
     router.get('/issues_opened', function(req, res) {
         request({
-            url: 'https://api.github.com/repos/' + req.query.ownerName + '/' + req.query.repoName + '/issues?state=all',
+            url: 'https://api.github.com/repos/' + req.query.ownerName + '/' + req.query.repoName + '/contributors',
             headers: { 'user-agent': 'git-technetium' },
             json: true
         }, function(error, response, body) {
             if(!error && response.statusCode === 200) {
-                res.send(body);
+                var contributors = [];
+                for(var contributorIndex = 0; contributorIndex < body.length; contributorIndex++){
+                    contributors.push(body[contributorIndex].login);
+                }
+
+                var contributorIssuesOpened = [];
+                for(var contributorIndex = 0; contributorIndex < contributors.length; contributorIndex++) {
+                    contributorIssuesOpened.push({
+                        'name': contributors[contributorIndex],
+                        'issues_opened': 0
+                    });
+                }
+
+                request({
+                    url: 'https://api.github.com/repos/' + req.query.ownerName + '/' + req.query.repoName + '/issues?state=all',
+                    headers: { 'user-agent': 'git-technetium' },
+                    json: true
+                }, function(error, response, body) {
+                    if(!error && response.statusCode === 200) {
+                        for(var issueIndex = 0; issueIndex < body.length; issueIndex++) {
+                            if(!body[issueIndex].pull_request) {
+                                for(var contributorIndex = 0; contributorIndex < contributorIssuesOpened.length; contributorIndex++) {
+                                    if(body[issueIndex].user.login === contributorIssuesOpened[contributorIndex].name) {
+                                        contributorIssuesOpened[contributorIndex].issues_opened++;
+                                    }
+                                }
+                            }
+                        }
+
+                        res.send(contributorIssuesOpened);
+                    }
+                });
             }
         });
     });
