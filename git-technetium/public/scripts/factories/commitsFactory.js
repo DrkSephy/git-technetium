e546e2e92fc2cf304459a03b37eb9bcf9dbd6010
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-28 22:01:51 -0400

    Adding factory for getting commits per contributor

---
+++ git-technetium/public/scripts/factories/commitsFactory.js
@@ -0,0 +1,16 @@
+'use strict';
+
+gitApp.factory('issuesFactory', function($http){
+    return {
+        get: function(owner, repo){
+            return $http({
+                url: '/api/commits',
+                method: 'GET',
+                params: {
+                    owner: owner,
+                    repo: repo
+                }
+            });
+        }
+    };
+});
