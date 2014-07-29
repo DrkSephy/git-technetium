247b36396248eef2832816da091178edd29a7818
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-28 22:26:01 -0400

    Adding controller for getting commits

---
+++ git-technetium/public/scripts/controllers/commitsController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+gitApp.controller('commitsController', function($scope, commitsFactory){
+    $scope.pageData = [];
+
+    $scope.submitQuery = function(){
+        $scope.pageData = commitsFactory.get($scope.owner, $scope.repo).success(function(data){
+            $scope.pageData = data;
+        });
+    }
+});
