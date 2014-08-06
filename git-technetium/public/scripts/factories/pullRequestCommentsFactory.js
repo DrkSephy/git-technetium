73b32eb0ab839f2748c4cc102335c527a80a7e45
WANKIM MOK <cafe.mui@gmail.com>
2014-08-05 23:26:01 -0400

    Added pull request comments per contributor factory

---
+++ git-technetium/public/scripts/factories/pullRequestCommentsFactory.js
@@ -0,0 +1,17 @@
+'use strict';
+
+gitApp.factory('pullRequestCommentsFactory', function($http){
+	return {
+		get: function(owner, repo){
+			return $http({
+				url: '/api/pullRequestComments',
+				methods: 'GET'.
+				params: {
+					owner: owner,
+					repo: repo
+				}
+
+			});
+		}
+	};
+});
