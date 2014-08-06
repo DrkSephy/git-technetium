561e781021f20fa14147a569c97b6b3939b1830b
David Leonard <sephirothcloud1025@yahoo.com>
2014-08-06 17:30:44 -0400

    Adding controller for lines of code

---
+++ git-technetium/public/scripts/controllers/locController.js
@@ -0,0 +1,12 @@
+'use strict';
+
+gitApp.controller('locController', function($scope, locFactory){
+    $scope.pageData = [];
+
+    $scope.submitQuery = function(){
+        $scope.pageData = locFactory.get($scope.owner, $scope.repo).success(function(data){
+            $scope.pageData = data;
+            console.log($scope.pageData);
+        });
+    }
+})
