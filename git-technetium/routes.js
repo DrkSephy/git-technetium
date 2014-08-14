f24b9b8300af40c6a5b4967b8544d67d8f6c426f
David Leonard <sephirothcloud1025@yahoo.com>
2014-08-14 16:25:36 -0400

    Extending issues_opened route to get all data

--- git-technetium/routes.js
+++ git-technetium/routes.js
@@ -64,25 +64,41 @@ module.exports = function(router, request, async) {
                     });
                 }
 
-                request({
-                    url: 'https://api.github.com/repos/' + req.query.ownerName + '/' + req.query.repoName + '/issues?state=all',
-                    headers: { 'user-agent': 'git-technetium' },
-                    json: true
-                }, function(error, response, body) {
-                    if(!error && response.statusCode === 200) {
-                        for(var issueIndex = 0; issueIndex < body.length; issueIndex++) {
-                            if(!body[issueIndex].pull_request) {
-                                for(var contributorIndex = 0; contributorIndex < contributorIssuesOpened.length; contributorIndex++) {
-                                    if(body[issueIndex].user.login === contributorIssuesOpened[contributorIndex].name) {
-                                        contributorIssuesOpened[contributorIndex].issues_opened++;
-                                    }
+                
+                var json = [];
+                var pageCounter = 1;
+
+                var getData = function(pageCounter){
+                    request({
+                        url: 'https://api.github.com/repos/' + req.query.ownerName + '/' + req.query.repoName + '/issues?state=all&page=' + pageCounter,
+                        headers: { 'user-agent': 'git-technetium' },
+                        json: true
+                    }, function(error, response, body) {
+                        if(!error && response.statusCode === 200) {
+                            for(var issueIndex = 0; issueIndex < body.length; issueIndex++) {
+                                if(!body[issueIndex].pull_request) {
+                                    json.push(body[issueIndex]);
                                 }
                             }
+                            if(body.length <30){
+                                for(var issueIndex = 0; issueIndex < json.length; issueIndex++){
+                                    for(var contributor_index = 0; contributor_index < contributorIssuesOpened.length; contributor_index++){
+                                        if(json[issueIndex].user.login === contributorIssuesOpened[contributor_index].name){
+                                            contributorIssuesOpened[contributor_index].issues_opened++;
+                                        }
+                                    }
+                                }
+                                res.send(contributorIssuesOpened);
+                            } else{
+                                getData(pageCounter + 1);
+                            }
+                            //res.send(urls);
                         }
+                    }); // end request
+                } // end getData function call
+                getData(1);
 
-                        res.send(contributorIssuesOpened);
-                    }
-                });
+
             }
         });
     });
