e48b9508f90dcd1cccd7557210badddfba812f48
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-16 10:58:14 -0400

    Updated issues server route to query all issues.

--- git-technetium/routes.js
+++ git-technetium/routes.js
@@ -18,27 +18,35 @@ module.exports = function(router, request, async, CLIENT_ID, CLIENT_SECRET) {
      *      An array, where each element contains the title of an issue in the repository
     **/
     router.get('/issues', function(req, res) {
-        request({
-            url: 'https://api.github.com/repos/' + req.query.owner + '/' + req.query.repo + '/issues?state=all' + '&' + 'client_id=' + CLIENT_ID + '&' + 'client_secret=' + CLIENT_SECRET,
-            headers: { 'user-agent': 'git-technetium' },
-            json: true
-        }, function(error, response, body) {
-            if(!error && response.statusCode === 200) {
-                var issueData = [];
-                for(var issueIndex = 0; issueIndex < body.length; issueIndex++) {
-                    if(!body[issueIndex].pull_request) {
-                        issueData.push({
-                            number: body[issueIndex].number,
-                            title: body[issueIndex].title,
-                            state: body[issueIndex].state,
-                            creator: body[issueIndex].user.login,
-                            assignee: body[issueIndex].assignee ? body[issueIndex].assignee.login : ''
-                        });
+        var issueData = [];
+        var getData = function(pageCounter) {
+            request({
+                url: 'https://api.github.com/repos/' + req.query.owner + '/' + req.query.repo + '/issues?state=all' + '&page=' + pageCounter + '&client_id=' + CLIENT_ID + '&' + 'client_secret=' + CLIENT_SECRET,
+                headers: { 'user-agent': 'git-technetium' },
+                json: true
+            }, function(error, response, body) {
+                if(!error && response.statusCode === 200) {
+                    for(var issueIndex = 0; issueIndex < body.length; issueIndex++) {
+                        if(!body[issueIndex].pull_request) {
+                            issueData.push({
+                                number: body[issueIndex].number,
+                                title: body[issueIndex].title,
+                                state: body[issueIndex].state,
+                                creator: body[issueIndex].user.login,
+                                assignee: body[issueIndex].assignee ? body[issueIndex].assignee.login : ''
+                            });
+                        }
+                    }
+
+                    if(body.length < 30) {
+                        res.send(issueData);
+                    } else {
+                        getData(pageCounter + 1);
                     }
                 }
-                res.send(issueData);
-            }
-        });
+            });
+        };
+        getData(1);
     });
 
     /**
