7a11b1ff9f0fc0a1d10251605c555b8649f6a194
David Leonard <sephirothcloud1025@yahoo.com>
2014-08-22 17:19:28 -0400

    Refactoring pull_requests route

---
+++ git-technetium/routes/pull_requests.js
@@ -0,0 +1,60 @@
+module.exports = function(router, request, async, config) { 
+    /**
+     * Route to query the total pull requests per contributor within a given repository
+     * params: ownerName, repoName
+    **/
+    router.get('/pulls', function(req, res){
+        request({
+            url: 'https://api.github.com/repos/'+req.query.owner + '/' + req.query.repo + '/contributors' + '?' + 'client_id=' + config.CLIENT_ID + '&' + 'client_secret=' + config.CLIENT_SECRET,
+            headers: {'user-agent' : 'git-technetium'},
+            json: true
+        }, function(error, response, body){
+            if(!error && response.statusCode === 200){
+                var contributors =[];
+
+                for (var contributor_index = 0; contributor_index < body.length; contributor_index++){
+                    contributors.push(body[contributor_index].login);
+                }
+
+                var contributor_tally =[];
+
+                for (var contributor_index = 0; contributor_index < body.length; contributor_index++){
+                    contributor_tally.push({
+                        'name' : contributors[contributor_index],
+                        'total': 0
+                    });
+                }
+
+                var json = [];
+                var pageCounter = 1;
+                var getData = function(pageCounter){
+                    request({
+                        url: 'https://api.github.com/repos/'+req.query.owner + '/' + req.query.repo + '/pulls?state=closed&page=' + pageCounter + '&' + 'client_id=' + config.CLIENT_ID + '&' + 'client_secret=' + config.CLIENT_SECRET,
+                        headers: {'user-agent' : 'git-technetium'},
+                        json: true
+                    }, function(error, response, body){
+                        if(!error && response.statusCode === 200){
+                            for (var pullsIndex = 0; pullsIndex < body.length; pullsIndex++){
+                                json.push(body[pullsIndex]);
+                            }
+                            if(body.length < 30){
+                                for(var pullsIndex = 0; pullsIndex < json.length; pullsIndex++){
+                                    for(var contributorIndex = 0; contributorIndex < contributors.length; contributorIndex++){
+                                        if(json[pullsIndex].user.login === contributor_tally[contributorIndex].name){
+                                            contributor_tally[contributorIndex].total++;
+                                        }
+                                    }
+                                }
+                                res.send(contributor_tally);
+                            } else {
+                                getData(pageCounter + 1);
+                            }
+
+                        }
+                    }); 
+                }
+                getData(1);
+            }
+        });
+    });
+}
