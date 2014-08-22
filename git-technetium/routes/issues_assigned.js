870e891f8e560ec2461384578dbb395d29ebb2e7
David Leonard <sephirothcloud1025@yahoo.com>
2014-08-22 17:18:23 -0400

    Refactoring issues_assigned route

---
+++ git-technetium/routes/issues_assigned.js
@@ -0,0 +1,65 @@
+module.exports = function(router, request, async, config) {    
+    /**
+     *  Precondition:
+     *      ownerName (string): The owner username of the target repository
+     *      repoName  (string): The target repository name
+     *  Postcondition:
+     *      An array of objects, where each object contains the following properties:
+     *          name (string): The contributor username
+     *          issues_opened (string): The number of issues assigned to the respective contributor
+    **/
+    router.get('/issues_assigned', function(req, res) {
+        request({
+            url: 'https://api.github.com/repos/' + req.query.owner + '/' + req.query.repo + '/contributors' + '?' + 'client_id=' + config.CLIENT_ID + '&' + 'client_secret=' + config.CLIENT_SECRET,
+            headers: { 'user-agent': 'git-technetium' },
+            json: true
+        }, function(error, response, body) {
+            if(!error && response.statusCode === 200) {
+                var contributors = [];
+                for(var contributorIndex = 0; contributorIndex < body.length; contributorIndex++){
+                    contributors.push(body[contributorIndex].login);
+                }
+
+                var contributorIssuesAssigned = [];
+                for(var contributorIndex = 0; contributorIndex < contributors.length; contributorIndex++) {
+                    contributorIssuesAssigned.push({
+                        'name': contributors[contributorIndex],
+                        'issues_assigned': 0
+                    });
+                }
+
+                var json = [];
+                var pageCounter = 1; 
+
+                var getData = function(pageCounter){
+                    request({
+                        url: 'https://api.github.com/repos/' + req.query.owner + '/' + req.query.repo + '/issues?state=all&page=' + pageCounter + '&' + 'client_id=' + config.CLIENT_ID + '&' + 'client_secret=' + config.CLIENT_SECRET,
+                        headers: { 'user-agent': 'git-technetium' },
+                        json: true
+                    }, function(error, response, body) {
+                        if(!error && response.statusCode === 200) {
+                            for(var issueIndex = 0; issueIndex < body.length; issueIndex++) {
+                                if(!body[issueIndex].pull_request) {
+                                    json.push(body[issueIndex]);
+                                }
+                            }
+                            if(body.length < 30){
+                                for(var issueIndex = 0; issueIndex < json.length; issueIndex++){
+                                    for(var contributorIndex = 0; contributorIndex < contributorIssuesAssigned.length; contributorIndex++){
+                                        if(json[issueIndex].assignee && json[issueIndex].assignee.login === contributorIssuesAssigned[contributorIndex].name){
+                                            contributorIssuesAssigned[contributorIndex].issues_assigned++;
+                                        }
+                                    }
+                                }
+                                res.send(contributorIssuesAssigned);
+                            } else{
+                                getData(pageCounter + 1);
+                            }
+                        }
+                    });
+                }
+                getData(1);
+            }
+        });
+    });
+}
