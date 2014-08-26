0335785bfbfdf7d746d702a5d91a5bcf54adf549
Kevin Chan <chessmasterhong@yahoo.com>
2014-08-25 21:18:15 -0400

    Fixed mixed spaces and tabs.

--- git-technetium/public/scripts/controllers/pullsController.js
+++ git-technetium/public/scripts/controllers/pullsController.js
@@ -4,8 +4,8 @@ gitApp.controller('pullsController', function($scope, pullsFactory) {
     $scope.pageData = [];
 
     $scope.submitQuery = function(){
-    	$scope.pageData = pullsFactory.get($scope.owner, $scope.repo).success(function(data){
-    		$scope.pageData = data;
-    	});
+        $scope.pageData = pullsFactory.get($scope.owner, $scope.repo).success(function(data){
+           $scope.pageData = data;
+        });
     }
 });
